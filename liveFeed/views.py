55ed9b827b1f06a22929f7249b2ded527618253a
Victor Moura <victor_cmoura@hotmail.com>
2017-10-17 23:37:03 -0200

    Configuring image capturing

--- liveFeed/views.py
+++ liveFeed/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import pygame
+import pygame.camera
+from django.http import JsonResponse
+import base64
+import json
 
 # Create your views here.
+
+def index(request):
+    pygame.camera.init()
+    cams = pygame.camera.list_cameras()
+    cam = pygame.camera.Camera(cams[0])
+    cam.start()
+    img = cam.get_image()
+    cam.stop()
+    pygame.image.save(img, "current_image.jpg")
+    image_data = open('current_image.jpg', 'rb').read()
+    
+    response_data = {}
+    response_data['encoded_image_data'] = base64.b64encode(image_data).decode('utf-8')
+    print(type(response_data['encoded_image_data']))
+    return JsonResponse(response_data)
+    #return HttpResponse(image_data, content_type='image/png')
+    
